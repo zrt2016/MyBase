e2eb18fec589192015bd1851466de6b847b5ff15
zhangrantuo <zhangrt@zpd.com>
2023-07-02 17:22:43 +0800

    BounceLoadingView 加载动画完善

--- kotlinapp/src/main/java/com/zrt/kotlinapp/activity_view/custom_view/loading_view/LoadingActivity.kt
+++ kotlinapp/src/main/java/com/zrt/kotlinapp/activity_view/custom_view/loading_view/LoadingActivity.kt
@@ -2,13 +2,18 @@ package com.zrt.kotlinapp.activity_view.custom_view.loading_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.zrt.kotlinapp.BasicActivity
 import com.zrt.kotlinapp.R
+import kotlinx.android.synthetic.main.activity_loading.*
 
 class LoadingActivity: BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+//        setContentView(R.layout.activity_loading)
     }
 
     override fun getLayoutResID(): Int {
@@ -16,8 +21,14 @@ class LoadingActivity: BasicActivity() {
     }
 
     override fun initData() {
+        a_load_bounce.setOnClickListener {
+            a_load_bounce.visibility = View.INVISIBLE
+        }
+        val lp = a_load_frame_bounce.layoutParams as LinearLayout.LayoutParams
+        lp.gravity = Gravity.CENTER
+        a_load_frame_bounce.layoutParams = lp
+        BounceLoadingView.attach(a_load_frame_bounce)
 
     }
-
 
 }
--- kotlinapp/src/main/java/com/zrt/kotlinapp/activity_view/custom_view/loading_view/LoadingViewUtils.kt
+++ kotlinapp/src/main/java/com/zrt/kotlinapp/activity_view/custom_view/loading_view/LoadingViewUtils.kt
@@ -1,18 +1,22 @@
 package com.zrt.kotlinapp.activity_view.custom_view.loading_view
 
+import android.R.attr.factor
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.zrt.kotlinapp.R
+
 
 /**
  * @author：Zrt
@@ -27,6 +31,24 @@ class BounceLoadingView: LinearLayout {
     // 阴影view
     lateinit var indication:ImageView
     val mTranslationYDistance: Float = 100f
+
+    lateinit var animatorSetFall: AnimatorSet
+    lateinit var animatorSetUp: AnimatorSet
+
+    val ANIM_DURATION:Long = 600
+
+    companion object {
+        // 代码添加
+        fun attach(parent: ViewGroup):BounceLoadingView {
+            val loadingView = BounceLoadingView(parent.context)
+            loadingView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+//            loadingView.gravity = Gravity.CENTER
+            parent.addView(loadingView)
+
+            return loadingView
+        }
+    }
+
     constructor(context: Context):this(context, null)
     constructor(context: Context, attrs: AttributeSet?):this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int):super(context, attrs, defStyleAttr) {
@@ -38,43 +60,90 @@ class BounceLoadingView: LinearLayout {
         shapeLoadingView2 = view.findViewById(R.id.shapeLoadingView2)
         indication = view.findViewById(R.id.indication)
 
-        val animatorSet = AnimatorSet()
+        initFallAnim()
+        initUpAnim()
+        fallAnimStart()
+    }
+    fun fallAnimStart() {
+        animatorSetFall.start()
+    }
+    // 初始化 下落动画
+    private fun initFallAnim() {
+        animatorSetFall = AnimatorSet()
         // 上下平移
         val shapTYAnima = ObjectAnimator.ofFloat(shapeLoadingView2, "translationY", 0f, mTranslationYDistance)
         shapTYAnima.interpolator = AccelerateInterpolator(1f)
-        shapTYAnima.repeatCount = ObjectAnimator.INFINITE
-        shapTYAnima.repeatMode = ObjectAnimator.REVERSE
+//        shapTYAnima.repeatCount = ObjectAnimator.INFINITE
+//        shapTYAnima.repeatMode = ObjectAnimator.REVERSE
         // 旋转动画
-        val shapRYAnima = ObjectAnimator.ofFloat(shapeLoadingView2, "rotation", 0f, 180f)
-        shapRYAnima.repeatCount = ObjectAnimator.INFINITE
-        shapRYAnima.repeatMode = ObjectAnimator.REVERSE
+//        val shapRYAnima = ObjectAnimator.ofFloat(shapeLoadingView2, "rotation", 0f, 180f)
+//        shapRYAnima.repeatCount = ObjectAnimator.INFINITE
+//        shapRYAnima.repeatMode = ObjectAnimator.REVERSE
         // 阴影缩放动画
-        val indicaScaleXAnima = ObjectAnimator.ofFloat(indication, "scaleX", 1f, 0.3f)
-        indicaScaleXAnima.repeatCount = ObjectAnimator.INFINITE
-        indicaScaleXAnima.repeatMode = ObjectAnimator.REVERSE
-        animatorSet.setDuration(1000)
-
-        animatorSet.playTogether(shapTYAnima, shapRYAnima, indicaScaleXAnima)
-        shapTYAnima.addListener(object : AnimatorListenerAdapter(){
+        val indicaScaleXAnima = ObjectAnimator.ofFloat(indication, "scaleX", 1f, 0.2f)
+//        indicaScaleXAnima.repeatCount = ObjectAnimator.INFINITE
+//        indicaScaleXAnima.repeatMode = ObjectAnimator.REVERSE
+        animatorSetFall.setDuration(ANIM_DURATION)
+        animatorSetFall.playTogether(shapTYAnima, indicaScaleXAnima)
+//        animatorSetFall.playTogether(shapTYAnima, shapRYAnima, indicaScaleXAnima)
+        animatorSetFall.addListener(object : AnimatorListenerAdapter() {
             //设置动画监听器，监听该动画的开始、停止、取消、结束等状态，我们往往会用AnimtorListener适配器类来只实现我们需要的方法
             override fun onAnimationEnd(animation: Animator?) {
-
+                super.onAnimationEnd(animation)
+                upAnimStart()
+                // 结束时，改变图像，然后执行上抛动画
+                shapeLoadingView2.changeGraph()
             }
 
             override fun onAnimationRepeat(animation: Animator?) {
-                // 重复播放时，改变颜色，然后执行上抛动画
-//                if (num%2 == 0) {
-//                    shapeLoadingView2.setBackground(ColorDrawable(Color.BLUE))
-//                }else {
-//                    shapeLoadingView2.setBackground(ColorDrawable(Color.GREEN))
-//                }
-//                num++
-                shapeLoadingView2.changeGraph()
+//                // 重复播放时，改变颜色，然后执行上抛动画
+//                shapeLoadingView2.changeGraph()
             }
         })
-        animatorSet.start()
     }
-    var num = 0;
+    fun upAnimStart() {
+        animatorSetUp.start();
+    }
+    // 初始化向上的动画
+    fun initUpAnim() {
+        animatorSetUp = AnimatorSet()
+        // 上下平移
+        val shapTYAnima = ObjectAnimator.ofFloat(shapeLoadingView2, "translationY", mTranslationYDistance, 0f)
+        shapTYAnima.interpolator = AccelerateInterpolator(1f)
+        // 阴影缩放动画
+        val indicaScaleXAnima = ObjectAnimator.ofFloat(indication, "scaleX", 0.2f, 1f)
+        animatorSetUp.setDuration(ANIM_DURATION)
+        animatorSetUp.playTogether(shapTYAnima, indicaScaleXAnima)
+        animatorSetUp.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                // 动画开始，播放旋转动画
+                shapeLoadingView2.startShapeRoteAnimator(ANIM_DURATION)
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                // 播放向上动画
+                fallAnimStart()
+            }
+        })
+    }
+
+    /**
+     * 隐藏优化
+     */
+    override fun setVisibility(visibility: Int) {
+        super.setVisibility(visibility)
+        when(visibility) {
+            View.GONE, View.INVISIBLE -> {
+                parent?.let {
+                    val vp = it as ViewGroup
+                    vp.removeView(this)
+                    shapeLoadingView2.clearAnimation()
+                    indication.clearAnimation()
+                    this.removeAllViews()
+                }
+            }
+        }
+    }
 }
 
 class GraphChangeLoadingView : View {
@@ -92,6 +161,8 @@ class GraphChangeLoadingView : View {
         mPaint.strokeWidth = 1f
         mPaint.style = Paint.Style.FILL_AND_STROKE
         mPath = Path()
+        // 初始化动画
+        initRotationAnim()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -118,7 +189,6 @@ class GraphChangeLoadingView : View {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         mPath.reset()
-
         when (graph) {
             0 -> {
                 mPaint.setColor(resources.getColor(R.color.orange))
@@ -126,11 +196,11 @@ class GraphChangeLoadingView : View {
             }
             1 -> {
                 mPaint.setColor(resources.getColor(R.color.green1))
-                mPath.addCircle(w/2f, h/2f, w/2f, Path.Direction.CW)
+                mPath.addCircle(w / 2f, h / 2f, w / 2f, Path.Direction.CW)
             }
             2 -> {
                 mPaint.setColor(resources.getColor(R.color.violet))
-                mPath.moveTo(w/2f, 0f)
+                mPath.moveTo(w / 2f, 0f)
                 mPath.lineTo(w.toFloat(), h.toFloat())
                 mPath.lineTo(0f, h.toFloat())
                 mPath.close()
@@ -143,5 +213,31 @@ class GraphChangeLoadingView : View {
         graph++
         if (graph > maxGraph) graph = 0
         postInvalidate()
+    }
+    lateinit var mRectRoteAnimation:ObjectAnimator
+    lateinit var mCircleRoteAnimation:ObjectAnimator
+    lateinit var mTriangleRoteAnimation:ObjectAnimator
+    fun initRotationAnim() {
+        mRectRoteAnimation = ObjectAnimator.ofFloat(this, "rotation", 0f, -120f)
+        mCircleRoteAnimation = ObjectAnimator.ofFloat(this, "rotation", 0f, 120f)
+        mTriangleRoteAnimation = ObjectAnimator.ofFloat(this, "rotation", 0f, 180f)
+    }
+    // 0、矩形；1、圆形；2、三角形
+    fun getUpThrowRoteAnimation(): ObjectAnimator {
+        return when(graph) {
+            1 -> mCircleRoteAnimation
+            2 -> mTriangleRoteAnimation
+            else -> mRectRoteAnimation
+        }
+    }
+
+    /**
+     * 执行旋转动画
+     */
+    fun startShapeRoteAnimator(duration:Long) {
+        val roteAnimation: ObjectAnimator = getUpThrowRoteAnimation()
+        roteAnimation.duration = duration
+        roteAnimation.interpolator = DecelerateInterpolator()
+        roteAnimation.start()
     }
 }
